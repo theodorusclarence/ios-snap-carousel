18648f9a2e65a42543af9293be4a661df8227191
Theodorus Clarence <theodorusclarence@gmail.com>
2022-05-16 13:27:39 +0700

    feat: initial commit

---
+++ movie-carousel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  movie-carousel
+//
+//  Created by Clarence on 16/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ movie-carousel/movie_carouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_carouselApp.swift
+//  movie-carousel
+//
+//  Created by Clarence on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct movie_carouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
